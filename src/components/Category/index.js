676c51b93fc5034854442c0184e38a4179ad3036
Adrien Vanderpotte <adrien@MacBook-Pro-de-Adrien.local>
2016-03-14 00:42:56 +0100

    FIX category page - images issue

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -8,10 +8,16 @@ export default Vue.extend({
   ready () {
     let key = this.$route.params.category
     this.data = Data[key]
+    this.data.drones.forEach((value, index) => {
+      let current = this.data.drones[index]
+      let imageUrl = `/static/img/categories/${this.$route.params.category}/${current.name.toLowerCase().replace(' ', '-')}.png`
+      current.image = imageUrl
+    })
   },
 
   data () {
     return {
+      coverUrl: '/static/img/categories/' + this.$route.params.category + '/drone.png',
       data: this.data
     }
   }
